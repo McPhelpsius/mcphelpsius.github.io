0a71436930644dbbb60f57337837e34c1fa3b4cf
Dr Money Handsomeson <nathanaphelps@gmail.com>
2015-06-29 20:50:09 -0500

    eeehhh, kitchen sink

---
+++ src/main.js
@@ -0,0 +1,16 @@
+let Amber = "the hottest girl in the world";
+let num = 0;
+let zero = 0;
+alert(Amber);
+
+function whoIsAmber (Amber) {
+  return Amber;
+}
+
+function addOne (num){
+  return num += 1;
+}
+
+//I'm leaving a bunch of comments
+//here at the bottom
+//to see them get built
